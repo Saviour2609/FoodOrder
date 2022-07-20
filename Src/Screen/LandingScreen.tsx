123d888d29f8b11321e20b1da89e83bb2bc37b71
AvigmaEmp <avigma.emp@gmail.com>
2022-07-20 15:47:14 +0530

    first commit

---
+++ Src/Screen/LandingScreen.tsx
@@ -0,0 +1,66 @@
+import react from 'react';
+import { View,Text,StyleSheet,Image,Dimensions } from 'react-native';
+
+const screenWidth = Dimensions.get('screen').width
+export const LandingScreen = () => {
+
+return (
+
+    <View style={styles.container}>
+    <View style={styles.navigation} /> 
+        
+    <View style={styles.body}>
+        <Image source={require('../Images/delivery_icon.png')} style={styles.deliveryIcon} />
+        <View style={styles.addressContainer}>
+            <Text style={styles.addressTitle}>Your Delivery Address</Text>
+        </View>
+        <Text style={styles.addressText}>Waiting for Current Location</Text>
+    </View>
+    
+</View>
+)
+}
+
+const styles = StyleSheet.create({
+
+  
+    container: {
+        flex: 1,
+        width:'100%',
+        backgroundColor: 'rgba(242,242,242,1)'
+     },
+    navigation: {
+        flex: 2,
+     },
+    body: {
+        flex: 9,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    deliveryIcon:{
+        width: 120,
+        height: 120
+    },
+    addressContainer: {
+        width: screenWidth - 100,
+        borderBottomColor: 'red',
+        borderBottomWidth: 0.5,
+        padding: 5,
+        marginBottom: 10,
+        alignItems: 'center',
+        
+    },
+    addressTitle:{
+        fontSize: 22,
+        fontWeight: '700',
+        color: '#7D7D7D'
+    },
+    addressText: {
+        fontSize: 20,
+        fontWeight: '200',
+        color: '#4F4F4F'
+    },
+
+    
+
+})
